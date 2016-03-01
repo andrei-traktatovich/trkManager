1e21224699661e83f3e47713fc7ee1db577cff0d
andrei nikolaev <andrei.nikolaew@gmail.com>
2016-03-01 17:32:24 +0300

    started adding globals service

--- TicketManager/Content/whowhere.spec/employeeListCtrl.spec.js
+++ TicketManager/Content/whowhere.spec/employeeListCtrl.spec.js
@@ -2,22 +2,67 @@
 /// <reference path="C:\Users\nikolaev\Documents\Visual Studio 2013\Projects\ticketManager 1702\TicketManager\Scripts/angular.min.js" />
 /// <reference path="C:\Users\nikolaev\Documents\Visual Studio 2013\Projects\ticketManager 1702\TicketManager\Scripts/angular-mocks.js" />
 
-var app = angular.module('testModule', []);
-app.factory('dataService', function () {
-    return {
-        isGood: true
-    }
-});
+'use strict';
 
-describe('testing', function () {
-    beforeEach(function () {
-        module('testModule');
+var globalsUrl = '/api/employeeCalendar/globals';
+
+(function () {
+     
+    angular.module('employeeCalendar.globals', [])
+    .service('globals', function ($http, $q) {
+        return {
+            get: function (name) {
+                var d = $q.defer();
+                d.resolve(1);
+                return d.promise;
+            }
+        }
     });
 
-    describe('just', function () {
-        it('works', inject(function (dataService) {
-            expect(dataService.isGood).toEqual(true);
-        }));
+})();
+
+
+describe('employeeCalendar.globals', function () {
+
+    var $httpBackEnd;
+    
+    beforeEach(function () {
+        module('employeeCalendar.globals');
     })
     
-})
+    beforeEach(inject(function ($injector) {
+       
+        $httpBackEnd = $injector.get('$httpBackend');
+        $httpBackEnd.when('GET', globalsUrl).respond({
+            offices: [{ id: 1, name: 'office1' }]
+        });
+    }));
+
+    describe('when asked for something', function () {
+
+        var _globalValues;
+
+        beforeEach(function(done) {
+            inject(function (globals) {
+                globals.get('offices').then(function (data) {
+
+                    _globalValues = data;
+                    done();
+                }, function () { done(); });
+            });
+        });
+
+        it('retrieves globals', function () {
+            expect(_globalValues).toBeDefined();
+        });
+
+        xit('retrieves globals only once', function () {
+            // expect $http call to be made only once 
+
+        });
+
+    });
+     
+   
+});
+ 
